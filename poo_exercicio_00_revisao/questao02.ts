a732922f5302934492f2a98583566e62d762be9f
francisco-igor <61854132+francisco-igor@users.noreply.github.com>
2021-08-24 23:51:57 -0300

    Create questao02.ts

---
+++ poo_exercicio_00_revisao/questao02.ts
@@ -0,0 +1,15 @@
+/*
+Escreva um programa para converter de dólar para real e exibir para o usuário a
+resposta final.
+*/
+
+const dolar = 5.25;
+
+var num = prompt('Valor em $: ');
+
+var real = parseFloat(num) * dolar;
+
+real = Math.floor(var1 * 100) / 100;
+
+console.clear()
+console.log(decimal('R$', real));
